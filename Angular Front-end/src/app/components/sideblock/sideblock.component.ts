8ae449376aa05a9ccf359dbff1a761a65d31c87c
MarLizZo <marketto140@libero.it>
2023-09-28 16:49:21 +0200

    head-foot + home responsive

--- Angular Front-end/src/app/components/sideblock/sideblock.component.ts
+++ Angular Front-end/src/app/components/sideblock/sideblock.component.ts
@@ -1,18 +1,22 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ISideBlockData } from 'src/app/interfaces/iside-block-data';
+import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-sideblock',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgbCollapseModule],
   templateUrl: './sideblock.component.html',
   styleUrls: ['./sideblock.component.scss'],
 })
 export class SideblockComponent {
   @Input() sideBlockData!: ISideBlockData;
+  isCollapsed: boolean = false;
+
+  @Input() collapseDefault!: boolean;
 
   ngOnInit() {
-    //
+    if (this.collapseDefault) this.isCollapsed = true;
   }
 }
